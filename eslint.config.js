370290e106f6f01a23c4a48c416b4d804f861e3e
Marcus Phan <58803185+MarcusPhan@users.noreply.github.com>
2025-09-13 18:35:01 +1000

    🚨 Fix
    eslint next

--- eslint.config.js
+++ eslint.config.js
@@ -2,15 +2,20 @@ import js from "@eslint/js";
 import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
-import tseslint from "typescript-eslint";
+import { defineConfig } from 'eslint/config';
+import pluginNext from '@next/eslint-plugin-next'
 
-export default tseslint.config(
-  { ignores: ["dist"] },
+export default defineConfig(
+  { ignores: ["dist", "src/components/ui"] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [
+      js.configs.recommended, 
+      pluginNext.configs.recommended,
+    ],
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
+      sourceType: "module",
       globals: globals.browser,
     },
     plugins: {
@@ -20,7 +25,7 @@ export default tseslint.config(
     rules: {
       ...reactHooks.configs.recommended.rules,
       "react-refresh/only-export-components": [
-        "warn",
+        "off",
         { allowConstantExport: true },
       ],
       "@typescript-eslint/no-unused-vars": "off",
